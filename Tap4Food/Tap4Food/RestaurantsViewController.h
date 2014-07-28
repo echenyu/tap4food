0aa13c0bd0e388a91b3f2d2648709d48be9a7779
Eric Yu <echenyu@umich.edu>
2014-07-28 15:13:52 -0400

    StoryBoard Changes

---
+++ Tap4Food/Tap4Food/RestaurantsViewController.h
@@ -0,0 +1,13 @@
+//
+//  RestaurantsViewController.h
+//  Tap4Food
+//
+//  Created by Eric Yu on 7/28/14.
+//  Copyright (c) 2014 EricYu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RestaurantsViewController : UIViewController
+
+@end
