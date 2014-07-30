71be18216bb8f042ce187e4ba5e72888dae77b92
Eric Yu <echenyu@umich.edu>
2014-07-30 02:18:56 -0400

    Finally got Yelp API to work,modified segue from Tap4FoodViewController to RestaurantsViewController. Changed up location of where things are called

---
+++ Tap4Food/Tap4Food/Model/Tap4FoodGenerateRestaurants.h
@@ -0,0 +1,16 @@
+//
+//  Tap4FoodGenerateRestaurants.h
+//  Tap4Food
+//
+//  Created by Eric Yu on 7/30/14.
+//  Copyright (c) 2014 EricYu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Tap4FoodGenerateRestaurants : NSObject
+
+-(void)generateRestaurants;
+@property (nonatomic, strong)NSDictionary *restaurantData;
+
+@end
