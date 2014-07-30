d7d01beecc7e8afc146e5a370b7041ba83e5c136
Eric Yu <echenyu@umich.edu>
2014-07-30 11:08:57 -0400

    working prototype. Brings up random restaurants in New York with the image, name, and phone number

--- Tap4Food/Tap4Food/RestaurantsViewController.m
+++ Tap4Food/Tap4Food/RestaurantsViewController.m
@@ -15,50 +15,45 @@
     NSDictionary *resultsFromYelp;
     NSArray *restaurantsFromYelp;
 }
-@property (nonatomic, strong)NSDictionary *yelpResults;
-@property (nonatomic, strong)NSMutableData *actualYelpResults;
-@property (nonatomic, strong)NSAttributedString *phoneNumbers;
-@property (nonatomic, strong)NSString *restaurantName;
-
+@property (weak, nonatomic) IBOutlet UITextField *phoneNumber;
+@property (weak, nonatomic) IBOutlet UILabel *restaurantName;
+@property (weak, nonatomic) IBOutlet UIImageView *restaurantPicture;
+@property (nonatomic, strong)UIActivityIndicatorView *activityIndicator;
+@property (nonatomic, strong)UIImage *restaurantImage;
 
 @end
 
 @implementation RestaurantsViewController
 
--(NSDictionary *)yelpResults {
-    if (!_yelpResults) {
-        _yelpResults = [[NSDictionary alloc]init];
+-(UIActivityIndicatorView *)activityIndicator {
+    if(!_activityIndicator) {
+        _activityIndicator = [[UIActivityIndicatorView alloc]initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+        _activityIndicator.center=CGPointMake(self.view.frame.size.width/2.0, self.view.frame.size.height/2.0);
     }
-    return _yelpResults;
+    return _activityIndicator;
 }
 
--(NSMutableData *)actualYelpResults {
-    if(!_actualYelpResults) {
-        _actualYelpResults = [[NSMutableData alloc]init];
+-(UIImage *)restaurantImage {
+    if (!_restaurantImage) {
+        _restaurantImage = [[UIImage alloc]init];
     }
-    return _actualYelpResults;
+    return _restaurantImage;
 }
 
--(NSAttributedString *)phoneNumbers {
-    if(!_phoneNumbers) {
-        _phoneNumbers = [[NSAttributedString alloc]init];
-    }
-    return _phoneNumbers;
-}
-
--(NSString *)restaurantName {
-    if(!_restaurantName) {
-        _restaurantName = [[NSString alloc]init];
-    }
-    return _restaurantName;
+-(void)viewWillAppear:(BOOL)animated {
+    [self.activityIndicator startAnimating];
+    [self.view addSubview:self.activityIndicator];
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    //[self setupRestaurants];
-    [self pickRandomRestaurant];
+    [self setupRestaurants];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(activitySpinnerEnd)
+                                                 name:@"NSURLSessionNotification"
+                                               object:nil];
 
 }
 
@@ -68,20 +63,59 @@
     // Dispose of any resources that can be recreated.
 }
 
-
+-(void)setupRestaurants {
+    //Use Oauth to authorize the user/the app to use Yelp's API
+    OAConsumer *consumer = [[OAConsumer alloc]initWithKey:@"D2WSy72QA5ilrtzEq7U-kg" secret:@"7WzTKp11knWN3dFT4MkjPjuiCk4"];
+    
+    OAToken *token = [[OAToken alloc]initWithKey:@"2wgDXaivi_U-uFRGL4jNMnsVkyQqoZPU" secret:@"LdGfbZ4Bi6Q3eW7zEWLPhIJWReM"];
+    
+    id<OASignatureProviding, NSObject> provider = [[OAHMAC_SHA1SignatureProvider alloc]init];
+    
+    NSURL *URL = [NSURL URLWithString:@"http://api.yelp.com/v2/search?term=restaurants&location=new%20york"];
+    
+    OAMutableURLRequest *request = [[OAMutableURLRequest alloc]initWithURL:URL
+                                                                  consumer:consumer
+                                                                     token:token
+                                                                     realm:nil
+                                                         signatureProvider:provider];
+    
+    [request setHTTPMethod:@"GET"];
+    [request prepare];
+    
+    NSURLSessionTask* task = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error){
+        NSDictionary *jsonDictionary = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
+        NSArray *businesses = [jsonDictionary objectForKey:@"businesses"];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            resultsFromYelp = jsonDictionary;
+            restaurantsFromYelp = businesses;
+            [self pickRandomRestaurant];
+            [[NSNotificationCenter defaultCenter] postNotificationName:@"NSURLSessionNotification" object:nil userInfo:nil];
+        });
+        
+    }];
+    
+    [task resume];
+}
 
 -(void)pickRandomRestaurant {
-    NSLog(@"IS there anything here yet %@", restaurantsFromYelp);
-    for (NSDictionary* dictionary in restaurantsFromYelp) {
-        NSLog(@"I need phone numbers: %@",[dictionary objectForKey:@"display_phone"]);
-    }
+    int numberOfRestaurants = (int)[restaurantsFromYelp count];
+    int randomRestaurantNumber = arc4random()%numberOfRestaurants;
+    NSLog(@"The random restaurant number is %i", randomRestaurantNumber);
+    
+    NSDictionary *pickedRestaurant = [restaurantsFromYelp objectAtIndex:randomRestaurantNumber];
+    NSURL *imageUrl = [[NSURL alloc]initWithString:[pickedRestaurant objectForKey:@"image_url"]];
+    NSData *imageData = [[NSData alloc]initWithContentsOfURL:imageUrl];
+    NSLog(@"random place %@", [pickedRestaurant objectForKey:@"name"]);
+    self.restaurantName.text = [pickedRestaurant objectForKey:@"name"];
+    self.phoneNumber.text = [pickedRestaurant objectForKey:@"display_phone"];
+    self.restaurantPicture.image = [UIImage imageWithData:imageData];
+
 }
 
-- (IBAction)button:(id)sender {
-//    NSLog(@"%@ stuff", self.yelpResults);
-//    NSLog(@"%@ other stuff", self.actualYelpResults);
+-(void)activitySpinnerEnd {
+    [self.activityIndicator stopAnimating];
 }
-
 
 /*
 #pragma mark - Navigation
