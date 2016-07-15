3394d2c74d56f19e553967a4932f4a0ea51d93cc
Eric Yu <echenyu@umich.edu>
2016-07-15 12:54:40 -0700

    ey - added Podfile for TDOAuth library

---
+++ Pods/Target Support Files/Pods-tap4Food/Pods-tap4Food-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_tap4Food : NSObject
+@end
+@implementation PodsDummy_Pods_tap4Food
+@end
---
+++ Pods/Target Support Files/Pods-tap4FoodUITests/Pods-tap4FoodUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_tap4FoodUITests : NSObject
+@end
+@implementation PodsDummy_Pods_tap4FoodUITests
+@end
