5c638fb2f7e871e4dacc18ccd3f1dbce315641a8
Eric Yu <echenyu@umich.edu>
2016-07-15 09:55:51 -0700

    Initial Commit

---
+++ tap4Food/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tap4Food
+//
+//  Created by Eric Yu on 7/15/16.
+//  Copyright © 2016 ericYu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tap4Food/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tap4Food
+//
+//  Created by Eric Yu on 7/15/16.
+//  Copyright © 2016 ericYu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
