0270e2a1b47627a6d5a96defc2fe2439d89778c9
Eric Yu <echenyu@umich.edu>
2016-07-15 22:43:24 -0700

    ey - added Google Maps to the restaurants view controller

--- tap4Food/ShowRestaurantsViewController.m
+++ tap4Food/ShowRestaurantsViewController.m
@@ -9,16 +9,26 @@
 #import "ShowRestaurantsViewController.h"
 #import "RestaurantCollection.h"
 #import "Restaurant.h"
+@import GoogleMaps;
 
 @interface ShowRestaurantsViewController () {
     UIActivityIndicatorView *activityIndicator;
     Restaurant *randomRestaurant;
+    GMSMapView *restaurantMapView;
 }
-@end
 
 //View Elements
+@property (nonatomic, weak) IBOutlet UILabel *phoneNumber;
+@property (nonatomic, weak) IBOutlet UILabel *restaurantName;
+@property (nonatomic, weak) IBOutlet UILabel *restaurantAddress;
+@property (nonatomic, weak) IBOutlet UILabel *numberOfRatings;
+@property (nonatomic, weak) IBOutlet UILabel *restaurantDescription;
+@property (nonatomic, weak) IBOutlet UILabel *distanceFromLocation;
+@property (nonatomic, weak) IBOutlet UIImageView *restaurantPicture;
+@property (nonatomic, weak) IBOutlet UIImageView *restaurantRating;
+@property (nonatomic, weak) IBOutlet UIView *restaurantMap;
 
-
+@end
 
 
 @implementation ShowRestaurantsViewController
@@ -37,12 +47,12 @@
 
 -(void) setupPage {
     [self setupActivityIndicator];
-//    [self setupRestaurants];
+    [self setupRestaurants];
 }
 
 -(void) setupActivityIndicator {
-    [self.view addSubview:activityIndicator];
-    [activityIndicator startAnimating];
+    [self.view addSubview:self.activityIndicator];
+    [self.activityIndicator startAnimating];
 }
 
 -(void) setupRestaurants {
@@ -53,12 +63,38 @@
         //Keep looping until dataLoaded is YES
         while([newCollection dataLoaded] == NO) {}
         randomRestaurant = [newCollection pickRandomRestaurant];
+        
         [self setupView];
     });
 }
 
 -(void) setupView {
+    [self.activityIndicator stopAnimating];
+    self.phoneNumber.text = randomRestaurant.phoneNumber;
+    self.restaurantName.text = randomRestaurant.restaurantName;
+    self.restaurantDescription.text = randomRestaurant.restaurantDescription;
+    self.numberOfRatings.text = [NSString stringWithFormat:@"%i reviews", randomRestaurant.numberOfRatings];
+    self.distanceFromLocation.text = [NSString stringWithFormat:@"%.2f miles", randomRestaurant.distanceFromLocation];
+    self.restaurantAddress.text = randomRestaurant.restaurantAddress;
+
+    self.restaurantPicture.image = [UIImage imageWithData:[[NSData alloc]initWithContentsOfURL:randomRestaurant.restaurantPictureURL]];
+    self.restaurantRating.image = [UIImage imageWithData:[[NSData alloc]initWithContentsOfURL: randomRestaurant.restaurantRatingImgUrl]];
+    [self setupMap];
+}
+
+-(void) setupMap {
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:_latitude
+                                                            longitude:_longitude
+                                                                 zoom:15];
+    restaurantMapView = [GMSMapView mapWithFrame:self.restaurantMap.bounds camera:camera];
+    restaurantMapView.myLocationEnabled = YES;
     
+    // Creates a marker in the center of the map.
+    GMSMarker *marker = [[GMSMarker alloc] init];
+    marker.position = CLLocationCoordinate2DMake(_latitude, _longitude);
+    marker.map = restaurantMapView;
+    
+    [self.restaurantMap addSubview: restaurantMapView];
 }
 
 -(UIActivityIndicatorView *)activityIndicator {
@@ -69,6 +105,11 @@
     return activityIndicator;
 }
 
+-(IBAction)openYelp:(id)sender {
+    if([[UIApplication sharedApplication]canOpenURL:randomRestaurant.restaurantURL]) {
+        [[UIApplication sharedApplication]openURL:randomRestaurant.restaurantURL];
+    }
+}
 /*
 #pragma mark - Navigation
 
